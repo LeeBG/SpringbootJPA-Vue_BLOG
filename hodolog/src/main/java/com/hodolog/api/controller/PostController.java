361540158a358506ee94535a86ce6277b708d3f0
LeeBG <donny1848@gmail.com>
2024-08-22 15:45:03 +0900

    데이터 콘텐트 타입

--- hodolog/src/main/java/com/hodolog/api/controller/PostController.java
+++ hodolog/src/main/java/com/hodolog/api/controller/PostController.java
@@ -1,10 +1,15 @@
 package com.hodolog.api.controller;
 
+import com.hodolog.api.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 // 컨트롤러 라우팅
 @RestController // 쉽게 데이터 응답이 가능하다. ResponseBody가 붙은  @Controller
+@Slf4j
 public class PostController {
     // SSR -> jsp, thymeleaf, mustache, freemarker
     // 서버에서 렌더링을 해가지고 렌더링
@@ -12,7 +17,7 @@ public class PostController {
     // SPA(Single Page Application) -> vue, react, nuxt, next
     // vue -> javascript에서 화면을 만들어주고 서버와의 통신은 API(JSON형태로 응답처리)
 
-//    @RequestMapping(method = RequestMethod.GET, path = "/vi/posts")
+    //    @RequestMapping(method = RequestMethod.GET, path = "/vi/posts")
     @GetMapping("/test")
     public String test() {
         return "안녕";
@@ -22,13 +27,26 @@ public class PostController {
     /*
      * HTTP 요청 메소드
      * GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT 등 이 있다.
-     * 글 등록 
+     * 글 등록
      * POST Method를 이용한다.
-     * 
+     *
      */
     // 게시글 작성
+//    @PostMapping("/posts")
+//    public String post(@RequestParam String title, @RequestParam String content) {
+//        log.info("title = {}, content = {}", title, content);
+//        return "Hello World";
+//    }
+
+//    @PostMapping("/posts")
+//    public String post(@RequestParam Map<String, String> params) {
+//        log.info("params = {}",params);
+//        return "Hello World";
+//    }
+
     @PostMapping("/posts")
-    public String post() {
+    public String post(@ModelAttribute PostCreate params) { // ModelAttribute 생략가능
+        log.info("params = {}", params);
         return "Hello World";
     }
 }
--- hodolog/src/test/java/com/hodolog/api/controller/PostControllerTest.java
+++ hodolog/src/test/java/com/hodolog/api/controller/PostControllerTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,14 +25,23 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 Hello World를 출려한다.")
     void test() throws Exception {
+        
+        // 글 제목, 글 내용
         // expected
-        mockMvc.perform(get("/posts"))
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+//                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE) // String으로 넘김
+                        .param("title", "글 제목입니다.")
+                        .param("content", "글 내용입니다. 하하")
+                )
                 .andExpect(status().isOk())
-                .andExpect(content().string("안녕"));
+                .andExpect(content().string("Hello World"))
+                .andDo(print());
     }
     /*
         mockMVC는 기본적으로 Content-Type을 application/json타입으로 보낸다.
-        서버에서 요청을 할때나 혹은 받을 때 받는 HTTP의 헤더값이다. 어떤 데이터의 형태다라고 알려준다.
+        서버에서 요청을 할때나 혹은 받을 때 받는 HTTP의 헤더값이다. 어떤 데이터의 형태 다라고 알려준다.
+        이전에는 application/x-www-form-urlencoded 많이 사용했다.
      */
 
 
