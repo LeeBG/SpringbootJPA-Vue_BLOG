60744ee7f0ec9939d0323e103d4c649cab69e26f
LeeBG <donny1848@gmail.com>
2024-09-19 23:50:01 +0900

    Spring Rest Docs3 - 커스터마이징

--- hodolog/src/test/java/com/hodolog/api/controller/PostControllerDocTest.java
+++ hodolog/src/test/java/com/hodolog/api/controller/PostControllerDocTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -23,6 +24,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -59,7 +61,7 @@ public class PostControllerDocTest {
                 .andDo(print())
                 // 응답필드에 대한 설명
                 .andExpect(status().isOk())
-                .andDo(document("index", 
+                .andDo(document("post-inquiry",
                         pathParameters(
                             parameterWithName("postId").description("게시글 ID")
                         ),
@@ -86,10 +88,11 @@ public class PostControllerDocTest {
                         .content(json))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index",
+                .andDo(document("post-create", // 글 조회
                         requestFields(
-                                fieldWithPath("title").description("제목"),
-                                fieldWithPath("content").description("내용")
+                                fieldWithPath("title").description("제목")
+                                        .attributes(key("constraint").value("좋은제목 입력해주세요")),
+                                fieldWithPath("content").description("내용").optional()
                         )
                 ));
     }
