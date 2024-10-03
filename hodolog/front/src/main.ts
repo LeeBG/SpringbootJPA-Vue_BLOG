59d52128a9a0530b921fd6be07dd8a2c2ad9de06
LeeBG <donny1848@gmail.com>
2024-10-03 23:11:12 +0900

    글 작성화면 만들기

--- hodolog/front/src/main.ts
+++ hodolog/front/src/main.ts
@@ -6,9 +6,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// ElementPlus 사용
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// Bootstrap의 Utiltities만 사용
+import 'bootstrap/dist/css/bootstrap-utilities.min.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
---
+++ hodolog/front/src/views/WriteView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import axios from "axios";
+
+// import {reactive, ref} from "vue";
+//
+//   // 일반 count 변수를 사용하면 갑 증가되지 않는다.
+//
+//   // 반응형 변수 선언
+//   const count = ref(0);
+//
+//   // Reactive 변수
+//   const obj = reactive({count});
+
+const title = ref('')
+const content = ref('')
+
+const write = function () {
+  axios.post("http://localhost:8080/posts", {
+    title: title.value,
+    content: content.value
+  });
+}
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" type="text" placeholder="제목을 입력해주세요" />
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15"/>
+  </div>
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성완료</el-button>
+  </div>
+</template>
+
+<style></style>
