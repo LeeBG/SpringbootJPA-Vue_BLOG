bb3fa1de0dee7ebb7bf83dbf6893b60fcf1100a8
LeeBG <donny1848@gmail.com>
2024-10-04 10:24:37 +0900

    CORS문제 해결 + WebConfig.java

--- hodolog/front/vite.config.ts
+++ hodolog/front/vite.config.ts
@@ -15,6 +15,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
+  // 어떤 요청을 하게되면 /posts로 시작하는 요청이 오면 localhost:8080으로 보내겠다고 설정을 할 수 있다.
+  // api 경로
+  server: {
+    proxy: {
+      "/api": { target:"http://localhost:8080",
+      rewrite: (path) => path.replace("/^\/api/",""),
+      },
+    },
+  },
 })
